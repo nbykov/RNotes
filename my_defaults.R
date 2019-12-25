d242958ad261a8c603c0089f0fcc30f45d6a1008
ntyresen <nikita@hum.aau.dk>
2019-12-25 15:25:14 +0100

    R 3.6.2

--- my_defaults.R
+++ my_defaults.R
@@ -3,9 +3,9 @@
 options(stringsAsFactors = FALSE) # Makes strings as factors default to FALSE
 options(repos = c(CRAN = "https://mirrors.dotsrc.org/cran/")) # Default CRAN
 
-# Write this to c:/Program Files/R/R-3.5.1/etc/Rprofile.site
+# Write this to c:/Program Files/R/R-3.6.2/etc/Rprofile.site
 beer <- function(save = "no") { q(save = save) }
 
 # Installing packages
-install.packages(c("tidyverse", "psych", "car", "MASS", "faraway"), lib = "C:/Program Files/R/R-3.5.1/library",
+install.packages(c("tidyverse", "psych", "car", "MASS", "faraway"), lib = "C:/Program Files/R/R-3.6.2/library",
   repos = "https://mirrors.dotsrc.org/cran/", dependencies = TRUE, verbose = TRUE)
