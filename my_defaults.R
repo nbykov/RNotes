06d49f8add3ed8a3f481e7c68c8ab7edccb9b9d3
Nik Tyresen <nik@hum.aau.dk>
2020-01-29 22:10:33 +0100

    edited defaults for better package installs

--- my_defaults.R
+++ my_defaults.R
@@ -1,23 +1,22 @@
 # My defaults for R :)
 
-options(stringsAsFactors = FALSE) # Makes strings as factors default to FALSE
-options(repos = c(CRAN = "https://mirrors.dotsrc.org/cran/")) # Default CRAN
+# ===***===
 
-# Write this to c:/Program Files/R/R-3.6.2/etc/Rprofile.site
+# Run this LOCALLY for reproducibility, when working with datasets with
+# a lot of character vectors
+options(stringsAsFactors = FALSE)
+
+# Write this to c:/Program Files/R/R-X.X.X/etc/Rprofile.site
 beer <- function(save = "no") { q(save = save) }
+
+local({
+  r = getOption("repos")
+  r["CRAN"] = "https://mirrors.dotsrc.org/cran/" # AAU Mirror
+  options(repos = r)
+})
+
+# ===***===
 
 # Installing packages
 install.packages(c("tidyverse", "psych", "car", "MASS", "faraway",
-  "devtools", "nortest"),
-  lib = "C:/Program Files/R/R-3.6.2/library",
-  repos = "https://mirrors.dotsrc.org/cran/", dependencies = TRUE, verbose = TRUE)
-
-library(devtools)
-  
-devtools::install_github("csgillespie/efficient",
-  lib = "C:/Program Files/R/R-3.6.2/library",
-  repos = "https://mirrors.dotsrc.org/cran/", dependencies = TRUE, verbose = TRUE)
-  
-devtools::install_github("hadley/r4ds",
-  lib = "C:/Program Files/R/R-3.6.2/library",
-  repos = "https://mirrors.dotsrc.org/cran/", dependencies = TRUE, verbose = TRUE)
+  "devtools", "nortest"), dependencies = TRUE, verbose = TRUE)
