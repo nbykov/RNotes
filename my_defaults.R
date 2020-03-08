0d84bb02ae63a99a736faa81a9f51f0b822219b7
Nik Tyresen <nik@hum.aau.dk>
2020-03-08 13:04:55 +0100

    Edited default wont in RGui and some other defaults

--- my_defaults.R
+++ my_defaults.R
@@ -2,9 +2,17 @@
 
 # ===***===
 
-# Run this LOCALLY for reproducibility, when working with datasets with
-# a lot of character vectors
+# Installing packages
+install.packages(c("tidyverse", "psych", "car", "MASS", "faraway",
+  "devtools", "nortest", "jtools"), dependencies = TRUE, verbose = TRUE)
+
+# ===***===
+
+# Run this LOCALLY (in the script preamble) for reproducibility, when working with datasets
+# with a lot of character vectors
 options(stringsAsFactors = FALSE)
+
+# ===***===
 
 # Write this to c:/Program Files/R/R-X.X.X/etc/Rprofile.site
 beer <- function(save = "no") { q(save = save) }
@@ -17,6 +25,18 @@ local({
 
 # ===***===
 
-# Installing packages
-install.packages(c("tidyverse", "psych", "car", "MASS", "faraway",
-  "devtools", "nortest", "jtools"), dependencies = TRUE, verbose = TRUE)
+# Edit this in c:/Program Files/R/R-X.X.X/etc/Rconsole
+
+## Style
+MDI = no
+
+## Font
+# https://stackoverflow.com/questions/14712401/set-font-and-font-size-in-r-console-programmatically)
+# https://www.johndcook.com/blog/2008/10/31/changing-the-r-console-fonts/
+font = Consolas
+points = 14
+
+## Language for messages
+language = "en"
+
+
