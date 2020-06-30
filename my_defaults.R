e7738388bb8af2ff8f5824b8469cbbcd0d6db9d7
Nik Tyresen <nik@hum.aau.dk>
2020-06-30 16:54:38 +0200

    Edits in my defaults

--- my_defaults.R
+++ my_defaults.R
@@ -2,19 +2,17 @@
 
 # ===***===
 
-# Installing packages
+## Installing packages
+# Run R with administrator privileges if installing to R shared library
+# Set mirror after installing a new version of R
+
 install.packages(c("tidyverse", "psych", "car", "MASS", "faraway",
   "devtools", "nortest", "jtools"), dependencies = TRUE, verbose = TRUE)
 
 # ===***===
 
-# Run this LOCALLY (in the script preamble) for reproducibility,
-# when working with datasets with a lot of character vectors
-options(stringsAsFactors = FALSE)
+## Write this to c:/Program Files/R/R-X.X.X/etc/Rprofile.site
 
-# ===***===
-
-# Write this to c:/Program Files/R/R-X.X.X/etc/Rprofile.site
 beer <- function(save = "no") { q(save = save) }
 
 local({
@@ -25,7 +23,7 @@ local({
 
 # ===***===
 
-# Edit this in c:/Program Files/R/R-X.X.X/etc/Rconsole
+## Edit this in c:/Program Files/R/R-X.X.X/etc/Rconsole
 
 ## Style
 MDI = no
@@ -33,8 +31,25 @@ MDI = no
 ## Font
 # https://stackoverflow.com/questions/14712401/set-font-and-font-size-in-r-console-programmatically)
 # https://www.johndcook.com/blog/2008/10/31/changing-the-r-console-fonts/
-font = Consolas
+font = TT Consolas
 points = 14
+
+# Dimensions (in characters) of the console.
+rows = 40
+columns = 100
+
+# Dimensions (in characters) of the internal pager.
+pgrows = 40
+pgcolumns = 100
 
 ## Language for messages
 language = "en"
+
+# ===***===
+
+## stringsAsFactors
+# Run this LOCALLY (in the script preamble) for reproducibility,
+# when working with datasets with a lot of character vectors
+options(stringsAsFactors = FALSE)
+
+# ===***===
